c074349ba28e6d46f52f80e9df31bb9651016819
Gregory Brown <gregory.t.brown@gmail.com>
2011-08-20 20:19:54 -0400

    Add update profile

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -8,8 +8,17 @@ class HooksController < ApplicationController
                   :mailchimp_web_id => params[:data][:web_id])
       render :text => "subscribed"
     when "unsubscribe"
-      User.find_by_mailchimp_web_id(params[:data][:web_id]).try(:destroy)
+      user = find_user
+
+      user.try(:destroy)
       render :text => "unsubscribed"
+    when "profile"
+      user = find_user
+
+      user.update_attributes(:first_name => params[:data][:merges][:FNAME],
+                             :last_name  => params[:data][:merges][:LNAME],
+                             :email      => params[:data][:email])
+      render :text => "update profile"
     else
       # ghetto
       if RAILS_ENV == "development"
@@ -18,5 +27,11 @@ class HooksController < ApplicationController
         render :text => "ok (unsupported)"
       end
     end
+  end
+
+  private
+  
+  def find_user
+    User.find_by_mailchimp_web_id(params[:data][:web_id])
   end
 end
