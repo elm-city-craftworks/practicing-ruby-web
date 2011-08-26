0a420947edff7ec1bc79de09f695d986796606ee
Gregory Brown <gregory.t.brown@gmail.com>
2011-08-26 15:57:43 -0400

    Articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_basic(
+       "A real auth system is coming soon. "+
+       "For now, use the email address you're using with "+
+       "MailChimp as your username. Password can be left blank.") do |id, password|
+      session[:seekrit] = password
+      User.where(:email => id).first
+    end
+  end
 end
