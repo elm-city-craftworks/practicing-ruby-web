aafe831e69ee1e439c2a86f29e0b292d7cc691c0
Gregory Brown <gregory.t.brown@gmail.com>
2011-08-29 14:16:50 -0400

    Switch to Redcarpet 2. Fix ridiculous haml issue

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,6 @@ module PracticingRubyWeb
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    Haml::Template.options[:ugly] = true
   end
 end
