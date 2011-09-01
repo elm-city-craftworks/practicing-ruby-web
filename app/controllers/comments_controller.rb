383fa833d63e89e933c5ffe4b39b52cabfe40f67
Jordan Byron <jordan@ducksoupsoftware.com>
2011-09-01 09:14:37 -0400

    Add comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,34 @@
+class CommentsController < ApplicationController
+  before_filter :find_comment,     :only => [:show, :update, :destroy]
+  before_filter :commentator_only, :only => [:update, :destroy]
+
+  def show
+    render :text => @comment.comment_text
+  end
+
+  def update
+    @comment.update_attribute(:comment_text, params[:value])
+
+    respond_to do |format|
+      format.text
+    end
+  end
+
+  def destroy
+    @comment.destroy
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def find_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def commentator_only
+    raise "Access Denied" if @comment.user != current_user
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
+  has_many :comments
+
   validates_presence_of   :mailchimp_web_id, :email
   validates_uniqueness_of :mailchimp_web_id, :email
+
   attr_protected :admin
 
   def name
