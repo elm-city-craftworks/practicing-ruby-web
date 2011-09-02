ae9fccd89e5f77366d5c77888a562a96cf6ff674
Gregory Brown <gregory.t.brown@gmail.com>
2011-09-02 14:23:05 -0400

    Allow fenced codeblocks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,8 @@ module ApplicationHelper
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
       :autolink            => true,
       :space_after_headers => true,
-      :no_intra_emphasis   => true)
+      :no_intra_emphasis   => true,
+      :fenced_code_blocks  => true)
 
     markdown.render(content).html_safe
   end
