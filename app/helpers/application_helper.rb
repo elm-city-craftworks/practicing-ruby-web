224399a1494cdba5d3c14f67fa31b133280ac2e5
Jordan Byron <jordan@ducksoupsoftware.com>
2011-09-09 16:23:37 -0400

    Add announcements admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,18 @@ module ApplicationHelper
 
     image_tag "http://www.gravatar.com/avatar/#{hash}?s=#{size}&d=mm"
   end
+
+  def error_messages_for(object)
+    if object.errors.any?
+      content_tag(:div, :id => "errorExplanation") do
+        content_tag(:h2) { "Whoops, looks like something went wrong." } +
+        content_tag(:p) { "Please review the form below and make the appropriate changes." } +
+        content_tag(:ul) do
+          object.errors.full_messages.map do |msg|
+            content_tag(:li) { msg }
+          end.join("\n").html_safe
+        end
+      end
+    end
+  end
 end
