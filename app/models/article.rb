2a791ad664aa4e2f1ae480b6751bd1cf011dd8a0
Jordan Byron <jordan@ducksoupsoftware.com>
2011-09-12 16:02:23 -0400

    Add article header styling

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ActiveRecord::Base
   has_many :comments, :as => :commentable
 
   validates_presence_of :issue_number
+
+  def full_subject
+    [issue_number, subject].join(": ")
+  end
 end
