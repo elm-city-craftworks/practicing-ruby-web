b122ca4b60f10dbae8238d95ad3171f39214295d
Jordan Byron <jordan@ducksoupsoftware.com>
2011-09-16 09:26:49 -0400

    Add delayed job and delayed_mailhopper

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110916131749) do
+ActiveRecord::Schema.define(:version => 20110916132222) do
 
   create_table "announcements", :force => true do |t|
     t.text     "title"
@@ -56,6 +56,21 @@ ActiveRecord::Schema.define(:version => 20110916131749) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "emails", :force => true do |t|
     t.string   "from_address",     :null => false
