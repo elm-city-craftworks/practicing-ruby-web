717ed6746034f9d6518ae79e7fceef7a3aaa4613
Jordan Byron <jordan@ducksoupsoftware.com>
2011-09-23 11:24:31 -0400

    First draft of header

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 PracticingRubyWeb::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+
+  root :to => 'home#index'
 
   match '/hooks/2f94d5ec414b463caa8d6f5f98bff105fd1b2151112' => 'hooks#receive'
 
   match '/articles/shared/:secret' => 'articles#shared', :as => "shared_article"
+
   resources :articles do
     member do
       get 'share'
