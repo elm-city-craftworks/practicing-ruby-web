3afdbb640a888f3d6260979c4013b353bed31173
Jordan Byron <jordan@ducksoupsoftware.com>
2011-09-26 16:20:09 -0400

    Fall back to asset pipeline until Rails 3.1.1 is released and fixes the following bug:
    https://github.com/rails/rails/issues/2765

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,11 +51,10 @@ PracticingRubyWeb::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
-
 
   config.assets.precompile += %w(landing.css)
 end
