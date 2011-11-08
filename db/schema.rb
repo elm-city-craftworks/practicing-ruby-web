cd90b6df12635c2d626fd761808f6c2ab282eb5d
Jordan Byron <jordan@ducksoupsoftware.com>
2011-11-08 13:04:41 -0500

    Add notify comment made to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110916140124) do
+ActiveRecord::Schema.define(:version => 20111108180149) do
 
   create_table "announcements", :force => true do |t|
     t.text     "title"
@@ -105,6 +105,7 @@ ActiveRecord::Schema.define(:version => 20110916140124) do
     t.boolean  "admin",                :default => false
     t.boolean  "notify_conversations", :default => true,  :null => false
     t.boolean  "notify_mentions",      :default => true,  :null => false
+    t.boolean  "notify_comment_made",  :default => false, :null => false
   end
 
 end
