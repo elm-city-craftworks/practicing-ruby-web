4f66b6f3a7e1543a5d2e1f8629e1d6cdd5b4e624
Jordan Byron <jordan@ducksoupsoftware.com>
2011-12-06 10:20:45 -0500

    Add broadcast tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'support/integration'
 require 'support/mini_contest'
 require 'test_notifier/runner/minitest'
 require "capybara/rails"
@@ -9,6 +10,17 @@ TestNotifier.silence_no_notifier_warning = true
 
 class ActionDispatch::IntegrationTest
   include Capybara::DSL
+  include Support::Integration
+
+  setup do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = {
+      'provider'  => "github",
+      'uid'       => '12345',
+      'user_info' => { 'nickname' => 'frankpepelio' }
+    }
+  end
+  teardown { Capybara.reset_sessions! }
 end
 
 OmniAuth.config.test_mode = true
