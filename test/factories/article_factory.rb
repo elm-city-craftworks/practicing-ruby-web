eb5abf5025416da7a76c46e0574423e1c6d4cfca
Jordan Byron <jordan@ducksoupsoftware.com>
2012-01-12 14:47:05 -0500

    Make tests green

--- test/factories/article_factory.rb
+++ test/factories/article_factory.rb
@@ -3,6 +3,6 @@ Factory.sequence(:article_subject) { |n| "Article #{n}" }
 Factory.define :article do |a|
   a.subject       { |_| Factory.next(:article_subject) }
   a.body          "Article Body"
-  a.issue_number  "Issue"
+  a.issue_number  "1"
   a.status        "published"
 end
