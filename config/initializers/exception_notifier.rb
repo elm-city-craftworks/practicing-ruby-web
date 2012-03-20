b8eb8fbdc8db5ab2761587882d791cfdef96c2ce
Gregory Brown <gregory.t.brown@gmail.com>
2012-03-20 19:48:53 +0000

    Remove what may be a line that's not doing anything.

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,6 +1,5 @@
 if Rails.env.production?
   PracticingRubyWeb::Application.config.middleware.use ExceptionNotifier,
     :email_prefix => "[Practicing Ruby] ",
-    :sender_address => %{"Practicing Ruby" <notifier@practicingruby.com>},
     :exception_recipients => %w{gregory.t.brown@gmail.com jordan.byron@gmail.com}
 end
