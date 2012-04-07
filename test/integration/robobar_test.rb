4581437d4c77e27ec1797ffc0f4638a10a106ef7
Jordan Byron <jordan.byron@gmail.com>
2012-04-07 15:50:31 -0400

    Add share panel and update tests

--- test/integration/robobar_test.rb
+++ test/integration/robobar_test.rb
@@ -23,4 +23,30 @@ class RobobarTest < ActionDispatch::IntegrationTest
 
     assert find('#robo-bar').visible?, "Robobar isn't visible"
   end
+
+  test "robobar has a share button" do
+    find('#chippy').click
+
+    within("#robo-bar") do
+      assert_content "Share with your friends"
+    end
+  end
+
+  test "share button opens the share panel and loads the share url" do
+    find('#chippy').click
+
+    find('#robo-bar button').click
+
+    assert find('#robo-share').visible?, "Share panel isn't visible"
+
+    skip "share doesn't exist right away and we need to wait a bit"
+
+    share = SharedArticle.where(
+      :article_id => @article.id, :user_id => @user.id
+    ).first
+
+    within("#robo-share") do
+      assert_equal find('input').value, shared_article_url(share.secret)
+    end
+  end
 end
