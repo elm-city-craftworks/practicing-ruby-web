1ba05f96054cc66d3a67ba802f9a0db5219bb3e8
Jordan Byron <jordan.byron@gmail.com>
2012-06-26 11:02:42 -0400

    Upgrade delayed_job and run migrations for 3.x changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120516152433) do
+ActiveRecord::Schema.define(:version => 20120626145819) do
 
   create_table "announcements", :force => true do |t|
     t.text     "title"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(:version => 20120516152433) do
     t.string   "locked_by"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "queue"
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
@@ -105,87 +106,6 @@ ActiveRecord::Schema.define(:version => 20120516152433) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  create_table "forem_categories", :force => true do |t|
-    t.string   "name",       :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "forem_forums", :force => true do |t|
-    t.string  "title"
-    t.text    "description"
-    t.integer "category_id"
-  end
-
-  create_table "forem_groups", :force => true do |t|
-    t.string "name"
-  end
-
-  add_index "forem_groups", ["name"], :name => "index_forem_groups_on_name"
-
-  create_table "forem_memberships", :force => true do |t|
-    t.integer "group_id"
-    t.integer "member_id"
-  end
-
-  add_index "forem_memberships", ["group_id"], :name => "index_forem_memberships_on_group_id"
-
-  create_table "forem_moderator_groups", :force => true do |t|
-    t.integer "forum_id"
-    t.integer "group_id"
-  end
-
-  add_index "forem_moderator_groups", ["forum_id"], :name => "index_forem_moderator_groups_on_forum_id"
-
-  create_table "forem_posts", :force => true do |t|
-    t.integer  "topic_id"
-    t.text     "text"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "reply_to_id"
-    t.string   "state",       :default => "pending_review"
-  end
-
-  add_index "forem_posts", ["reply_to_id"], :name => "index_forem_posts_on_reply_to_id"
-  add_index "forem_posts", ["state"], :name => "index_forem_posts_on_state"
-  add_index "forem_posts", ["topic_id"], :name => "index_forem_posts_on_topic_id"
-  add_index "forem_posts", ["user_id"], :name => "index_forem_posts_on_user_id"
-
-  create_table "forem_subscriptions", :force => true do |t|
-    t.integer "subscriber_id"
-    t.integer "topic_id"
-  end
-
-  create_table "forem_topics", :force => true do |t|
-    t.integer  "forum_id"
-    t.integer  "user_id"
-    t.string   "subject"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "locked",       :default => false,            :null => false
-    t.boolean  "pinned",       :default => false
-    t.boolean  "hidden",       :default => false
-    t.datetime "last_post_at"
-    t.string   "state",        :default => "pending_review"
-  end
-
-  add_index "forem_topics", ["forum_id"], :name => "index_forem_topics_on_forum_id"
-  add_index "forem_topics", ["state"], :name => "index_forem_topics_on_state"
-  add_index "forem_topics", ["user_id"], :name => "index_forem_topics_on_user_id"
-
-  create_table "forem_views", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "topic_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "count",      :default => 0
-  end
-
-  add_index "forem_views", ["topic_id"], :name => "index_forem_views_on_topic_id"
-  add_index "forem_views", ["updated_at"], :name => "index_forem_views_on_updated_at"
-  add_index "forem_views", ["user_id"], :name => "index_forem_views_on_user_id"
 
   create_table "shared_articles", :force => true do |t|
     t.integer  "user_id"
@@ -205,14 +125,12 @@ ActiveRecord::Schema.define(:version => 20120516152433) do
     t.datetime "updated_at"
     t.text     "github_nickname"
     t.boolean  "admin",                 :default => false
-    t.boolean  "notify_conversations",  :default => true,             :null => false
-    t.boolean  "notify_mentions",       :default => true,             :null => false
-    t.boolean  "notify_comment_made",   :default => false,            :null => false
+    t.boolean  "notify_conversations",  :default => true,  :null => false
+    t.boolean  "notify_mentions",       :default => true,  :null => false
+    t.boolean  "notify_comment_made",   :default => false, :null => false
     t.boolean  "beta_tester",           :default => false
     t.boolean  "account_disabled",      :default => false
-    t.boolean  "forem_admin",           :default => false
-    t.string   "forem_state",           :default => "pending_review"
-    t.boolean  "notifications_enabled", :default => false,            :null => false
+    t.boolean  "notifications_enabled", :default => false, :null => false
   end
 
   create_table "volumes", :force => true do |t|
