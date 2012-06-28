1dd2a36b7a0e0e0f3db8a82b892b78bd9ac11f5b
Jordan Byron <jordan.byron@gmail.com>
2012-06-28 16:33:51 -0400

    Use bundle exec rake in crontab [ci skip]

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,9 @@
 set :output, "~/practicing-ruby/current/log/cron_log.log"
 
+# Make sure +bundle exec+ is used when executing rake tasks
+#
+job_type :rake, "cd :path && RAILS_ENV=:environment bundle exec rake :task --silent :output"
+
 every 10.minutes do
   rake "mailchimp:disable_unsubscribed"
 end
