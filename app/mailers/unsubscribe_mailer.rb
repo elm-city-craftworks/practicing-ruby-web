f9949480064e313a6d9e653e479a6cafeafe117a
Jordan Byron <jordan.byron@gmail.com>
2012-07-23 14:05:00 -0400

    Use new subscribe path

--- app/mailers/unsubscribe_mailer.rb
+++ app/mailers/unsubscribe_mailer.rb
@@ -1,5 +1,7 @@
 class UnsubscribeMailer < ActionMailer::Base
   def unsubscribed(address)
+    @address = address
+
     mail(:to      => address,
          :subject => "Sorry to see you go").deliver
   end
