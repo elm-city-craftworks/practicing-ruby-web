4a024f27244efc375f15f18a6cb54cb96cbef758
Gregory Brown <gregory.t.brown@gmail.com>
2012-09-23 09:24:53 -0400

    Fix 404 rendering

---
+++ test/integration/404_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class MissingPageTest < ActionDispatch::IntegrationTest
+  test "responds with a 404 on missing pages" do
+    visit "/volume/next-fed63d91b398e5ede229dd9bde03286d.png"
+
+    assert_match "doesn't exist", page.body
+
+    assert_equal 404, page.driver.status_code
+  end
+end
