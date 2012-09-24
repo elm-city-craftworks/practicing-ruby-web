c833a24d957745c44cfc6f9352aab1007668ff10
Gregory Brown <gregory.t.brown@gmail.com>
2012-09-24 11:13:24 -0400

    use a different filename convention

--- app/models/article.rb
+++ app/models/article.rb
@@ -35,7 +35,15 @@ class Article < ActiveRecord::Base
   private
 
   def document
-    Struct.new(:body).new(
-      MdPreview::Parser.parse(File.read(Rails.root + "app/documents/#{issue_number}.md")))
+    volume, issue, part = issue_number.split(".")
+    
+    identifier = "v#{volume}/#{issue.rjust(3, '0')}"
+    identifier << ".#{part}" if part
+
+
+    filename = Dir.glob(Rails.root + "app/documents/#{identifier}*.md").first
+
+
+    Struct.new(:body).new(MdPreview::Parser.parse(File.read(filename)))
   end
 end
