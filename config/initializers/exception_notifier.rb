268f9f74e47390760f9df4f1987529a5d2c4c0a8
Gregory Brown <gregory.t.brown@gmail.com>
2012-09-30 10:55:44 -0400

    Do we pass actual exceptions?

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -2,5 +2,5 @@ if Rails.env.production?
   PracticingRubyWeb::Application.config.middleware.use ExceptionNotifier,
     :email_prefix => "[Practicing Ruby] ",
     :exception_recipients => %w{gregory.t.brown@gmail.com jordan.byron@gmail.com},
-    :ignore_exceptions    => ['ActionView::MissingTemplate'] + ExceptionNotifier.default_ignore_exceptions
+    :ignore_exceptions    => [ActionView::MissingTemplate] + ExceptionNotifier.default_ignore_exceptions
 end
