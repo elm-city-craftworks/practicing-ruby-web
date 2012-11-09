685329ebeb979b37f365f2e16979999cc8c414ab
Gregory Brown <gregory.t.brown@gmail.com>
2012-11-09 15:16:43 -0500

    I hate Ruby 1.9.2

--- test/factories/credit_card.rb
+++ test/factories/credit_card.rb
@@ -1,8 +1,6 @@
 FactoryGirl.define do
-  sequence(:credit_card_number) { rand(1000..9999).to_s }
-
   factory :credit_card do |c|
-    c.last_four        { FactoryGirl.generate(:credit_card_number) }
+    c.last_four("1234")
     c.expiration_month { Date.today.month }
     c.expiration_year  { Date.today.year  }
     c.user
