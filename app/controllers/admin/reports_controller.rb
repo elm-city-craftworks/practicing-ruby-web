2cca445551c3d9a1e497c17bc32bcb67c1e10526
Gregory Brown <gregory.t.brown@gmail.com>
2012-11-24 18:50:16 -0500

    Extract activity report into a model

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -5,31 +5,7 @@ module Admin
     def index
       @statuses    = User.select("status, count(*) as user_count").group("status")
       @total_users = User.count
-      @activity    = activity
-    end
-
-    private
-
-    def activity
-      query = %{ article_visits.updated_at > ? and 
-                 users.status = 'active' }
-
-      report = {}
-      
-      [1,3,7,14,21,28].each do |days|
-        date = Date.today - days
-        
-        q="start_date < :date and (finish_date is null or finish_date > :date)"
-
-        subscribers = Subscription.where(q, :date => date).count.to_f
- 
-        report[days] = ArticleVisit.includes("user")
-                                   .where(query, date)
-                                   .group(:user_id).count.count / subscribers
-
-      end
-
-      report.map { |k,v| "#{k}: #{'%.2f' % v}" }.join(" | ")
+      @activity    = ActivityReport.call
     end
   end
 end
