c6fe753ee8ebd8f78cf34d46c69b26fa9f6f0630
Gregory Brown <gregory.t.brown@gmail.com>
2013-07-18 07:43:34 -0400

    Reduce activation to at least two days in first month

--- app/models/activation_report.rb
+++ app/models/activation_report.rb
@@ -4,7 +4,7 @@ ActivationReport = ->() {
 
   active_accounts = new_subscribers.select do |e| 
                       ArticleVisit.where(:user_id => e.id)
-                                  .map { |e| e.created_at.to_date }.uniq.count > 2 
+                                  .map { |e| e.created_at.to_date }.uniq.count > 1 
                     end
 
   percentage = "%.2f" % (active_accounts.count.to_f / new_subscribers.count)
