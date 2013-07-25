53989a6d5fd90cd0780fe89c3cbab213fda8cc2c
Gregory Brown <gregory.t.brown@gmail.com>
2013-07-24 21:31:19 -0400

    Exclude author from all comment emails

--- app/models/conversation_notifier.rb
+++ app/models/conversation_notifier.rb
@@ -26,15 +26,7 @@ class ConversationNotifier
   attr_accessor :comment, :author, :article
 
   def comment_watchers
-    watchers = users.where(:notify_comment_made => true)
-
-    if mentioned_watchers.any?
-      # no need to send a comment notification because they'll
-      # get a mentioned one
-      watchers.where("users.id NOT IN(?)", mentioned_watchers)
-    else
-      watchers
-    end
+    users.where(:notify_comment_made => true)
   end
 
   def conversation_watchers
@@ -46,6 +38,12 @@ class ConversationNotifier
   end
 
   def users
-    User.where("id != ?", author.id)
+    relation = User.where("id != ?", author.id)
+
+    if mentioned_watchers.any?
+      relation.where("users.id NOT IN(?)", mentioned_watchers)
+    else
+      relation
+    end
   end
 end
