edd1383152a84af7fe32866bd1a57fec8c1f1488
Jordan Byron <jordan.byron@gmail.com>
2013-08-29 16:24:45 -0400

    Now that god is on the new server, we can kick it on and off

--- config/deploy/new.rb
+++ config/deploy/new.rb
@@ -8,13 +8,13 @@ set :user, "deploy"
 server "new.practicingruby.com", :app, :web, :db, :primary => true
 
 before 'deploy:update_code' do
-  #run "sudo god stop practicing_ruby_delayed_job"
+  run "sudo god stop practicing_ruby_delayed_job"
 end
 
 after 'deploy' do
-  #run  "sudo god load #{release_path}/config/delayed_job.god"
-  #run  "sudo god start practicing_ruby_delayed_job"
-  #run_rake "bake:articles"
+  run  "sudo god load #{release_path}/config/delayed_job.god"
+  run  "sudo god start practicing_ruby_delayed_job"
+  # run_rake "bake:articles"
 end
 
 after 'deploy:restart', 'unicorn:restart'
