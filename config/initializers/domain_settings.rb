d57f8c17c4594ddfc73a29e4eeae877f2e4bf345
Gregory Brown <gregory.t.brown@gmail.com>
2013-10-23 09:46:35 -0400

    No longer need domain settings example file

--- config/initializers/domain_settings.rb
+++ config/initializers/domain_settings.rb
@@ -1,4 +1,4 @@
-url_options = { :host => "practicingruby.dev" }
+url_options = { :host => ENV["HOST"] }
 
 ActionMailer::Base.default_url_options       = url_options
 Rails.application.routes.default_url_options = url_options
