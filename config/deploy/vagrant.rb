c37b129186d203760a076ebca88ee242a7ef5919
Gregory Brown <gregory.t.brown@gmail.com>
2013-10-24 10:13:51 -0400

    Add task to import articles into vagrant, based on canned DB dump

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -17,4 +17,21 @@ set :default_environment, { "PATH" => "/opt/rubies/2.0.0-p247/bin:$PATH" }
 # Set custom hostname to connect to Vagrant VM
 server "practicingruby.local", :app, :web, :db, :primary => true
 
+desc "Import articles, volumes, and collections from the server"
+namespace :import do
+  task :articles do
+    remote_db = remote_database_config
+    file = "dump.sql.bz2"
+
+    `rsync db/#{file} #{user}@#{find_servers.first.host}:/tmp/#{file}`
+
+    run %{ bzcat /tmp/dump.sql.bz2 | } +
+        %{PGPASSWORD=#{remote_db['password']} }  +
+        %{psql -U#{remote_db['username']} -hlocalhost #{remote_db['database']}}
+  end
+end
+   
+
+
+
 after "deploy:restart", "unicorn:restart"
